97267b0175b9e8eef284484eeddcc2a61a5fa088
Bennett <110846042+bennett-sh@users.noreply.github.com>
2023-10-02 12:46:04 +0200

    feat: option.map + option documentation

--- src/option.ts
+++ src/option.ts
@@ -1,5 +1,19 @@
 import { UnwrapError } from './utils/errors.js'
 
+/**
+ * A type for representing a value that may not exist.
+ * @example
+ * async function getUser(): Promise<Option<User>> {
+ *  const result = await fetch('/user')
+ *  const user: User | null = await result.json()
+ *
+ *  return Option.from(user)
+ * }
+ *
+ * const user = await getUser()
+ *
+ * user.map(u => u.name).unwrap() //=> string | undefined
+*/
 export class Option<T> {
   public static some<T>(data: T)  {
     return new Option(true, data)
@@ -7,6 +21,7 @@ export class Option<T> {
   public static get none(): Option<any> {
     return new Option(false, undefined)
   }
+  /** Creates an option from a value T, which maybe null or undefined. */
   public static from<T>(data: T | undefined | null): Option<T> {
     if(data === undefined || data === null) return Option.none
     return Option.some(data)
@@ -25,22 +40,37 @@ export class Option<T> {
   }
 
 
+  /** Returns the actual value or undefined if it's an Option.none */
   public unwrap(): T | undefined {
     return this.data
   }
 
+  /** Returns the actual value or throws an unwrap error. */
   public unwrap$(): T | never {
     if(!this._isSome) throw new UnwrapError('attempted to unwrap on a no-value option')
     return this.data
   }
 
+  /** Returns the actual value or the result of a callback, which may also throw an error */
   public unwrapOrElse(f: () => T | never): T {
     if(!this._isSome) return f()
     return this.unwrap()
   }
 
+  /** Returns the actual value or the first parameter */
   public unwrapOr(or: T): T {
     if(!this._isSome) return or
     return this.unwrap()
+  }
+
+  /**
+   * Allows you to safely use the value without checking whether it exists
+   * @example
+   * const user: Option<User> = getUser();
+   * const userName = user.map(({ name }) => name).unwrap$(); // note how we delay the unwrapping to end of the chain
+  */
+  public map<TNew>(f: (value: T) => TNew): Option<TNew> {
+    if(this.isNone) return Option.none
+    return Option.some(f(this.unwrap$()))
   }
 }
